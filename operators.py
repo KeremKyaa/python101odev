b935259df5e7f6a5e13d1b2b706d356e61d58c07
krmky117 <krmky227@gmail.com>
2025-12-26 18:01:27 +0300

    operators

---
+++ operators.py
@@ -0,0 +1,12 @@
+sayi1, sayi2 = 40, 15
+print(f"Toplam: {sayi1 + sayi2}, Fark: {sayi1 - sayi2}, Çarpım: {sayi1 * sayi2}, Bölüm: {sayi1 / sayi2}")
+
+sayi_gir = int(input("Bir sayı girin: "))
+print(f"100'den büyük mü? {sayi_gir > 100}")
+print(f"100'e eşit mi? {sayi_gir == 100}")
+print(f"100'den küçük mü? {sayi_gir < 100}")
+
+kullanici_adi = "admin"
+sifre = "12345"
+print(kullanici_adi == "admin" and sifre == "12345")
+
