585b2ecd9e1894cface425f13e3acb53647fa971
krmky117 <krmky227@gmail.com>
2025-12-26 17:58:15 +0300

    print fonksiyonu ve ozellikleri

---
+++ main.py
@@ -0,0 +1,20 @@
+print("Merhaba, Python Dünyası!")
+print("Python", "öğrenmek", "çok", "eğlenceli", sep="-")
+
+print("Adım:", end=" ")
+
+print("Gemini")
+
+print("Python Dersleri:\n\t- Konu 1\n\t- Konu 2")
+
+"""
+aşağıdaki program değişken atama ve 
+yazdırma işlemini gösterir
+"""
+
+isim = "Ahmet" # isim değişkenine string bir değer atar
+yas = 25       # yas değişkenine integer bir değer atar
+
+print(isim)    # isim değişkenini ekrana yazdırır
+print(yas)     # yas değişkenini ekrana yazdırır
+
