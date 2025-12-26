bd60bf7276b1398a2519b22c898d8808d74388c8
krmky117 <krmky227@gmail.com>
2025-12-26 18:00:19 +0300

    math

---
+++ math.py
@@ -0,0 +1,11 @@
+# math fonksiyonu ile daire alanı ve çevre bulma
+
+import math
+r = 7
+alan = math.pi * math.pow(r, 2)
+cevre = 2 * math.pi * r
+print(f"Alan: {alan}, Çevre: {cevre}")
+
+#liste fonksiyonları
+liste = [15, 4, 27, 13, 32, 8]
+print(f"En Büyük: {max(liste)}, En Küçük: {min(liste)}, Toplam: {sum(liste)}")
