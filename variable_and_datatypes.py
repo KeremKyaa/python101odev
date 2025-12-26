18424042c410d2d606c942a3f9e67ddcf94c5e5a
krmky117 <krmky227@gmail.com>
2025-12-26 18:02:09 +0300

    variable and data types

---
+++ variable_and_datatypes.py
@@ -0,0 +1,11 @@
+ad = "Kerem Kaya" # string tipinde
+dogum_yili = 2004    # integer tipinde
+boy = 1.90           # float tipinde
+ogrenci_mi = True    # boolean tipinde
+
+print(f"Benim adım {ad}, {dogum_yili} yılında doğdum. Boyum {boy} metre ve öğrencilik durumum: {ogrenci_mi}.")
+
+favori_meyveler = ["Erik", "Muz", "Çilek"] # List
+araba_bilgileri = ("Tesla", "Model S", 2023) # Tuple
+ogrenci_notlari = {85, 90, 78} # Set
+kisi_bilgileri = {"ad": "Kerem", "soyad": "Kaya", "sehir": "Bursa"} # Dict
