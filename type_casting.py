cd3d27cfd692aca4214e1af3e4abd6d4d362c8c9
krmky117 <krmky227@gmail.com>
2025-12-26 18:01:45 +0300

    type casting

---
+++ type_casting.py
@@ -0,0 +1,7 @@
+yil_str = "1923"
+sonuc = int(yil_str) + 100
+print(sonuc)
+
+yas = input("Lütfen yaşınızı giriniz: ")
+on_yil_sonra = int(yas) + 10
+print(f"10 yıl sonra {on_yil_sonra} yaşında olacaksınız.")
